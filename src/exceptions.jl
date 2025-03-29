14c07fb574e88772556a5723b6e8a63863139244
Andrea Baroffio <andrea.baroff2000@gmail.com>
2025-03-29 09:27:12 +0100

    add docstrings in exceptions.jl

--- src/exceptions.jl
+++ src/exceptions.jl
@@ -1,9 +1,21 @@
-# Defining personalized Exceptions
-# PFM
+"""
+    struct WrongPFMformat <: Exception
+Custom exception for handling incorrect PFM file format errors.
+
+# Fields
+- `msg::String`: Error message describing the issue.
+"""
 struct WrongPFMformat <: Exception
     msg::String
 end
-# Tone Mapping
+
+"""
+    struct ToneMappingError <: Exception
+Custom exception for errors encountered during tone mapping operations.
+
+# Fields
+- `msg::String`: Error message describing the issue.
+"""
 struct ToneMappingError <: Exception
     msg::String
 end
