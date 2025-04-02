a1e567411db5a71040c976a863079eee5263bafc
Stefano Bozzi <stefano.bozzi@gmail.com>
2025-04-02 12:11:43 +0200

    create vector, point and normal structs

---
+++ geometry.jl
@@ -0,0 +1,18 @@
+# Vectors
+mutable struct Vec
+    x::Real
+    y::Real
+    z::Real
+end
+# Point
+mutable struct Point
+    x::Real
+    y::Real
+    z::Real
+end
+# Normal
+mutable struct Normal
+    x::Real
+    y::Real
+    z::Real
+end
