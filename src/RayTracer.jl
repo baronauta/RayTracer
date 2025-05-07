388cb2cdbaed18c97fcf5cd9fab0b11c3c151528
Stefano Bozzi <stefano.bozzi@gmail.com>
2025-05-07 17:44:43 +0200

    feat: add preliminary `demo.jl` for rendering two LDR images
    Add an initial version of demo.jl which renders two LDR .png images:
    one using a perspective camera, the other using an orthographic camera.
    Image width, height, aspect ratio, tone mapping parameters, and observer
    location are currently fixed.
    
    Future versions will introduce configurable parameters and additional features.

--- src/RayTracer.jl
+++ src/RayTracer.jl
@@ -30,6 +30,8 @@ export +,
     write_ldr_image,
     GeometryError
 
+export HdrImage, OrthogonalCamera, PerspectiveCamera, translation, scaling,
+        Vec, ImageTracer, Sphere, World, fire_ray, RGB, ray_intersection, fire_all_rays!
 const little_endian = Base.ENDIAN_BOM == 0x04030201 # true if the host is little endian, false otherwise
 
 include("exceptions.jl")
