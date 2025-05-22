928f8c42e99eb7408a93ca51bd8bc9e07a55b0ba
Andrea Baroffio <andrea.baroff2000@gmail.com>
2025-05-22 16:08:52 +0200

    add random_float! and improve docstrings

--- src/pcg.jl
+++ src/pcg.jl
@@ -3,18 +3,12 @@ mutable struct PCG
     inc::UInt64
 end
 
-function random!(pcg::PCG)
-    oldstate = pcg.state
-    # Update the state of the PCG
-    pcg.state = oldstate * 6364136223846793005 + pcg.inc
-    # Return UInt32 computed as follows, i.e. a number in range [0, 2³² - 1].
-    # ⊻ is the xor operator in julia
-    xorshifted = UInt32((((oldstate >> 18) ⊻ oldstate) >> 27) & typemax(UInt32))
-    rot = UInt32(oldstate >> 59)
-    return UInt32((xorshifted >> rot) | (xorshifted << ((-rot) & 31)))
-end
+"""
+    PCG(; init_state=42, init_seq=54)
 
-"Initialize the PCG."
+Create and initialize a PCG (Permuted Congruential Generator) random number generator.
+`init_state` and `init_seq` are optional seeds.
+"""
 function PCG(;init_state=42, init_seq=54)
     # Throw a random number and discard it
     state = 0
@@ -28,4 +22,28 @@ function PCG(;init_state=42, init_seq=54)
     return pcg
 end
 
+"""
+    random!(pcg::PCG) -> UInt32
 
+Generate a new 32-bit random number using the PCG algorithm,
+updating the internal state.
+"""
+function random!(pcg::PCG)::UInt32
+    oldstate = pcg.state
+    # Update the state of the PCG
+    pcg.state = oldstate * 6364136223846793005 + pcg.inc
+    # Return UInt32 computed as follows, i.e. a number in range [0, 2³² - 1].
+    # ⊻ is the xor operator in julia
+    xorshifted = UInt32((((oldstate >> 18) ⊻ oldstate) >> 27) & typemax(UInt32))
+    rot = UInt32(oldstate >> 59)
+    return UInt32((xorshifted >> rot) | (xorshifted << ((-rot) & 31)))
+end
+
+"""
+    random_float!(pcg::PCG) -> Float64
+
+Generate a random floating-point number in [0, 1) using PCG.
+"""
+function random_float!(pcg::PCG)::Float64
+    return Float64(random!(pcg)) / (UInt32(typemax(UInt32)) + 1)
+end
