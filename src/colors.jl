b74289c0c59a097f8a18498acbdecc4d2ac3b654
Stefano Bozzi <stefano.bozzi@gmail.com>
2025-05-22 17:01:18 +0200

    chore: add summarized license notice to files

--- src/colors.jl
+++ src/colors.jl
@@ -1,3 +1,28 @@
+
+#_______________________________________________________________________________________
+#     LICENSE NOTICE: European Union Public Licence (EUPL) v.1.2
+#     __________________________________________________________
+#
+#   This file is licensed under the European Union Public Licence (EUPL), version 1.2.
+#
+#   You are free to use, modify, and distribute this software under the conditions
+#   of the EUPL v.1.2, as published by the European Commission.
+#
+#   Obligations include:
+#     - Retaining this notice and the licence terms
+#     - Providing access to the source code
+#     - Distributing derivative works under the same or a compatible licence
+#
+#   Full licence text: see the LICENSE file or visit https://eupl.eu
+#
+#   Disclaimer:
+#     Unless required by applicable law or agreed to in writing,
+#     this software is provided "AS IS", without warranties or conditions
+#     of any kind, either express or implied.
+#
+#_______________________________________________________________________________________
+
+
 "Wrapper function to ColorTypes.RGB{Float32}."
 function RGB(r, g, b)
     ColorTypes.RGB{Float32}(r, g, b)
