f7eeebf4ec9a925b4261974aa69a3286c8f061a4
Andrea Baroffio <andrea.baroff2000@gmail.com>
2025-05-23 12:10:31 +0200

    test: CheckeredPigment

--- test/material_tests.jl
+++ test/material_tests.jl
@@ -9,4 +9,15 @@
         @test RayTracer.get_color(pigment, Vec2D(0.0, 1.0)) ≈ color
         @test RayTracer.get_color(pigment, Vec2D(1.0, 1.0)) ≈ color
     end
+
+    @testset "CheckeredPigment" begin
+        color1 = RGB(1.0, 2.0, 3.0)
+        color2 = RGB(10.0, 20.0, 30.0)
+        pigment = CheckeredPigment(color1, color2, 2)
+
+        @test RayTracer.get_color(pigment, Vec2D(0.25, 0.25)) ≈ color1
+        @test RayTracer.get_color(pigment, Vec2D(0.75, 0.25)) ≈ color2
+        @test RayTracer.get_color(pigment, Vec2D(0.25, 0.75)) ≈ color2
+        @test RayTracer.get_color(pigment, Vec2D(0.75, 0.75)) ≈ color1
+    end
 end
