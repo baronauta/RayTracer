fd22d252244d14edef30cc0d28913832a9eae767
Stefano Bozzi <stefano.bozzi@gmail.com>
2025-05-26 10:26:51 +0200

    feat: create Flat Renderer

--- src/render.jl
+++ src/render.jl
@@ -1,13 +1,19 @@
-abstract type Renderer end
 # ─────────────────────────────────────────────────────────────
 # ON-OFF RENDERER
 # ─────────────────────────────────────────────────────────────
-struct OnOffRederer <: Renderer
-    # code
+function OnOff_Tracer(world::World, ray::Ray; bkg_color = BLACK)
+    isnothing(ray_intersection(world, ray)) ? bkg_color : WHITE
 end
+
+
 # ─────────────────────────────────────────────────────────────
 # FLAT RENDERER
 # ─────────────────────────────────────────────────────────────
-struct FlatRederer <: Renderer
-    # code
+function Flat_Tracer(world::World, ray::Ray; bkg_color = BLACK)
+    hit_record = ray_intersection(world, ray)
+    isnothing(hit_record) ? bkg_color :
+    (
+        get_color(hit_record.shape.material.brdf.pigm, hit_record.surface_point) +
+        get_color(hit_record.shape.material.emitted_radiance, hit_record.surface_point)
+    )
 end
